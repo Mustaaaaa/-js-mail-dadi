107e0e8e56f39fa18e612922550d756fd6f23a3b
mustaaaaa <moustafaibrahim1512@gmail.com>
2024-03-07 19:26:06 +0100

    add: random number

--- js/app.js
+++ js/app.js
@@ -22,12 +22,55 @@ for (let i = 0; i < emailsconstentite.length; i++){
 /* se l'utente ha l'email giusta e ha l'accesso constentito allora gioca 
 al gioco dei dadi altrimenti deve ricaricare la pagina per scrivere 
 un'altra email */
+const cpusubmit = document.getElementById('cpu')
+const giocatoresubmit = document.getElementById('giocatore')
 if (emailcritta == false) {
     console.log('email senza permesso di accesso');
 }
-else{
+else {
     console.log('email con permesso di accesso');
+    const numbers = [];
 
+    giocatoresubmit.addEventListener('click', function getRndInteger(min, max) {
+        const dado1 = document.querySelector('.dado1')
+        const dado2 = document.querySelector('.dado2')
+        for (let iterations = 1; iterations <= 6; iterations++) {
+            const randomNumbercpu = Math.floor((Math.random() * (7 - 0)));
+            const randomNumbergiocatore = Math.floor((Math.random() * (7 - 0)));
+
+            numbers.push(randomNumbercpu);
+            numbers.push(randomNumbergiocatore);
+
+            dado2.innerHTML = `
+		    <div class="dado">${randomNumbercpu}</div>
+		    `
+            dado1.innerHTML = `
+            <div class="dado">${randomNumbergiocatore}</div>
+            `
+            if (randomNumbergiocatore > randomNumbercpu) {
+                const pElement = document.getElementsByClassName('p')
+                let winner = pElement
+                pElement.innerHTML = `
+                <p class="risultato">"ha vinto il giocatore</p>
+                `
+                console.log(winner)
+            }
+            else if (randomNumbercpu > randomNumbergiocatore) {
+                const pElement = document.getElementsByClassName('p')
+                let winner = pElement
+                pElement.innerHTML = `
+                <p class="risultato">${"hhofg"} </p>
+                `
+                console.log(winner)
+            }
+        }
+        
+        console.log(numbers);
+        
+    })
+    
+    
+
+    console.log(emailsconstentite);
 }
-console.log(emailsconstentite);
 
